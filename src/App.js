7be247bb9d9734524107615cab2f4a7027f8cc83
Julia Matsievich <juliamatsievich@mail.ru>
2023-08-11 23:38:43 +0500

    добавляет компонент sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Player } from './components/audioPlayer/audioPlayer';
 import { NavMenu } from './components/navMenu/navMenu';
 import { TrackList } from './components/trackList/trackList';
+import { SideBar } from './components/sideBar/sideBar';
 
 function App() {
   return (
@@ -10,47 +11,7 @@ function App() {
             <main className="main">
           <NavMenu />
           <TrackList />
-              <div className="main__sidebar sidebar">
-                <div className="sidebar__personal">
-                  <p className="sidebar__personal-name">Sergey.Ivanov</p>
-                  <div className="sidebar__icon">
-                    <svg alt="logout">
-                      <use xlinkHref="img/icon/sprite.svg#logout"></use>
-                    </svg>
-                  </div>
-                </div>
-                <div className="sidebar__block">
-                  <div className="sidebar__list">
-                    <div className="sidebar__item">
-                      <a className="sidebar__link" href="#">
-                        <img
-                          className="sidebar__img"
-                          src="img/playlist01.png"
-                          alt="day's playlist"
-                        />
-                      </a>
-                    </div>
-                    <div className="sidebar__item">
-                      <a className="sidebar__link" href="#">
-                        <img
-                          className="sidebar__img"
-                          src="img/playlist02.png"
-                          alt="day's playlist"
-                        />
-                      </a>
-                    </div>
-                    <div className="sidebar__item">
-                      <a className="sidebar__link" href="#">
-                        <img
-                          className="sidebar__img"
-                          src="img/playlist03.png"
-                          alt="day's playlist"
-                        />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <SideBar/>
             </main>
           <Player />
             <footer className="footer"></footer>
