ac4eefdbb4e3465d6962ea27bdcc1365b17e3f62
Julia Matsievich <juliamatsievich@mail.ru>
2023-08-21 16:09:18 +0500

    категории

--- src/components/trackList/filter/filter.jsx
+++ src/components/trackList/filter/filter.jsx
@@ -1,40 +1,90 @@
 import './filter.css'
-import { FilterAuthor, FilterYear, FilterGanr } from './filterCategory'
+import {
+  FilterAuthor,
+  FilterYear,
+  FilterGanr,
+  FilterCategory,
+  FilterCat,
+  Filters
+} from './filterCategory'
 import { useState } from 'react'
+import { FilterCategoryAuthor } from './filterCategoryAuthor'
+import { FilterCategoryYear } from './FilterCategoryYear'
+import { FilterCategoryGanr } from './FilterCategoryGanr'
 
 export function Filter() {
-  //   const [isActiveIndex, setActiveIndex] = useState(false)
-  let [author, setAuthor] = useState(false)
-  let [year, setYear] = useState(false)
-  let [ganr, setGanr] = useState(false)
+  let [isActiveIndex, setActiveIndex] = useState(0)
+  // const [isActive, setActive] = useState(false)
 
-  if (author === true) {
-    setYear(year = true)
-    setGanr(ganr = false)
-  }
+  // const [open, setOpen] = useState(author);
+  //   let [author, setAuthor] = useState(false)
+  //   let [year, setYear] = useState(false)
+  //   let [ganr, setGanr] = useState(false)
 
-  if (year === true) {
-    setAuthor(author = false)
-    setGanr(ganr = false)
-  }
+  //   if (author === true) {
+  //     setYear(year = true)
+  //     setGanr(ganr = false)
+  //   }
 
-  if (ganr === true) {
-    setYear(year = false)
-    setAuthor(author = false)
-  }
+  //   if (year === true) {
+  //     setAuthor(author = false)
+  //     setGanr(ganr = false)
+  //   }
+
+  //   if (ganr === true) {
+  //     setYear(year = false)
+  //     setAuthor(author = false)
+  //   }
 
   return (
     <div className="centerblock__filter filter">
       <div className="filter__title">Искать по:</div>
-      <FilterAuthor
-        isOpen={author}
-        onShow={() => setAuthor((author) => {
-			console.log(author);
-			return !author
-		  } )}
+      {/* <FilterAuthor
+        isOpen={isActiveIndex === true}
+        onShow={() => setActiveIndex(!isActiveIndex)}
       />
-      <FilterYear isOpen={year} onShow={() => setYear((year) => !year)} />
-      <FilterGanr isOpen={ganr} onShow={() => setGanr((ganr) => !ganr)} />
+      <FilterYear
+        isOpen={isActiveIndex === true}
+        onShow={() => setActiveIndex(!isActiveIndex)}
+      />
+      <FilterGanr
+        isOpen={isActiveIndex === true}
+        onShow={() => setActiveIndex(!isActiveIndex)}
+      /> */}
+
+      {/* <FilterCategory
+			isOpen={isActiveIndex === true}
+			onShow={() => {
+        setActiveIndex(!isActiveIndex)
+      }
+       }
+			year={isActiveIndex === true}
+			author={isActiveIndex === true}
+			ganr={isActiveIndex === true}
+		/> */}
+
+      <FilterCat
+        isOpen={isActiveIndex}
+        onShow={() => {
+            setActiveIndex((isActiveIndex = 1))
+        }}
+        title="исполнителю"
+      />
+      <FilterCat
+        isOpen={isActiveIndex === 0}
+        onShow={() => {
+          setActiveIndex((isActiveIndex = 2))
+        }}
+        title="году выпуска"
+      />
+      <FilterCat
+        isOpen={isActiveIndex === 0}
+        onShow={() => {
+          setActiveIndex((isActiveIndex = 3))
+        }}
+        title="жанру"
+      />
+ 
     </div>
   )
 }
--- src/components/trackList/filter/filterCategory.jsx
+++ src/components/trackList/filter/filterCategory.jsx
@@ -1,32 +1,68 @@
-import './filterCategory.css';
-import { FilterCategoryAuthor } from './filterCategoryAuthor';
-import { FilterCategoryYear } from './FilterCategoryYear';
-import { FilterCategoryGanr } from './FilterCategoryGanr';
+import './filterCategory.css'
+import { FilterCategoryAuthor } from './filterCategoryAuthor'
+import { FilterCategoryYear } from './FilterCategoryYear'
+import { FilterCategoryGanr } from './FilterCategoryGanr'
 
 export function FilterAuthor({ isOpen, onShow }) {
   return (
     <div onClick={onShow} className="filter__button button-author _btn-text">
       исполнителю
-      {isOpen ? <FilterCategoryAuthor/> : null}
+      {isOpen ? <FilterCategoryAuthor /> : null}
     </div>
   )
 }
 
-export function FilterYear({ year, onShow }) {
+export function FilterYear({ isOpen, onShow }) {
   return (
     <div onClick={onShow} className="filter__button button-year _btn-text">
       году выпуска
-      {year ? <FilterCategoryYear/> : null}
+      {isOpen ? <FilterCategoryYear /> : null}
     </div>
   )
 }
 
-export function FilterGanr({ ganr, onShow }) {
+export function FilterGanr({ isOpen, onShow }) {
   return (
-    <div onClick={onShow} className="filter__button button-genre _btn-text"
-    >
+    <div onClick={onShow} className="filter__button button-genre _btn-text">
       жанру
-		{ganr ? <FilterCategoryGanr/> : null }
+      {isOpen ? <FilterCategoryGanr /> : null}
     </div>
   )
 }
+
+export function FilterCategory({ isOpen, onShow, author, year, ganr }) {
+  return (
+    <>
+      <div onClick={onShow} className="filter__button button-author _btn-text">
+        исполнителю
+        {author ? <FilterCategoryAuthor /> : null}
+		  {year || ganr && null}
+      </div>
+      <div onClick={onShow} className="filter__button button-year _btn-text">
+        году выпуска
+      {year ? <FilterCategoryYear /> : null}
+		  {author || ganr && null}
+      </div>
+
+      <div onClick={onShow} className="filter__button button-genre _btn-text">
+        жанру
+        {ganr ? <FilterCategoryGanr /> : null}
+		  {year || author && null}
+      </div>
+    </>
+  )
+}
+
+export function FilterCat({ isOpen, onShow, title}) {
+  return (
+    <div onClick={onShow} className="filter__button button-genre _btn-text">
+      {title}
+      
+      {isOpen === 1 ? <FilterCategoryAuthor /> : null}
+      {isOpen === 2 ? <FilterCategoryYear /> : null}
+      {isOpen === 3 ? <FilterCategoryGanr /> : null}
+
+    </div>
+  )
+}
+
