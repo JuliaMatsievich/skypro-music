72fe2529f198ecfe7435a9662cdf5f0fc3df29d8
Julia Matsievich <juliamatsievich@mail.ru>
2023-08-30 23:20:38 +0500

    добавляет стили к favorites

--- src/pages/favorites/index.jsx
+++ src/pages/favorites/index.jsx
@@ -1,7 +1,17 @@
+import * as S from './index.styles'
+import { NavMenu } from '../../components/navMenu/navMenu'
+
 export function Favorites() {
 	return (
-		<div>
-      <h1>Мои треки</h1>
-    </div>
+		<S.Container>
+		<S.Main>
+		  <NavMenu />
+		  <S.MainCenterBlock>
+		  <h1>Мои треки</h1>
+		  </S.MainCenterBlock>
+			
+		</S.Main>
+		<S.Footer></S.Footer>
+	 </S.Container>
 	)
 }
