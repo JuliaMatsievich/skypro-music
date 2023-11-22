7654aaa0479a2cc0604454b8fdb81695dce31662
JuliaMatsievich <juliamatsievich@mail.ru>
2023-11-23 00:26:45 +0500

    починила лайки

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -5,11 +5,8 @@ import { UserContext } from '../../App'
 import { useGetAllTracksQuery } from '../../services/trackApi'
 import { Filter } from '../../components/filter/filter'
 import { HeaderTrackList } from '../../components/headerTrackListAndSearch/headerTrackList'
-import { filterAuthor, filterGenre } from '../../helpers/filterFunc'
 import { searchMusic } from '../../helpers/searchFunc'
 import { sortTracks } from '../../helpers/sortFunc'
-import { currentTrackSelector } from '../../store/trackSlice'
-import { useSelector } from 'react-redux'
 
 export const MainPage = () => {
   const { logOut, allTracksError, setAllTracksError } = useContext(UserContext)
@@ -19,11 +16,7 @@ export const MainPage = () => {
   const [authorFilter, setAuthorFilter] = useState([])
   const [genreFilter, setGenreFilter] = useState([])
   const [search, setSearch] = useState('')
-  // const [filterTracks, setFilterTracks] = useState([])
   const [defaultPlaylist, setDefaultPlaylist] = useState([])
-  const currentPlaylist = useSelector(
-    (state) => state.audioPlayer.currentPlaylist,
-  )
 
   const handleChangeFilter = (type, value) => {
     if (type === 'author') {
@@ -42,82 +35,6 @@ export const MainPage = () => {
     }
   }
 
-  // const handleChangeFilter = (type, value) => {
-  //   if (type === 'author') {
-  //     if (genreFilter.length === 0) {
-  //       if (authorFilter.includes(value)) {
-  //         // setFilterTracks(filterTracks.filter(({ author }) => author !== value))
-  //         setAuthorFilter(authorFilter.filter((item) => item !== value))
-  //       } else {
-  //         // setFilterTracks([...filterTracks, ...filterAuthor(data, value)])
-  //         setAuthorFilter([...authorFilter, value])
-  //       }
-  //     }
-
-  //     if (genreFilter.length !== 0) {
-  //       if (authorFilter.includes(value)) {
-  //         // setFilterTracks(filterTracks.filter(({ author }) => author !== value))
-  //         setAuthorFilter(authorFilter.filter((item) => item !== value))
-  //         if (authorFilter.length === 1) {
-  //           // setFilterTracks(
-  //           //   data.filter((track) => genreFilter.indexOf(track.genre) > -1),
-  //           // )
-  //         }
-  //       }
-  //       if (authorFilter.length === 0) {
-  //         // setFilterTracks(filterAuthor(filterTracks, value))
-  //         setAuthorFilter([...authorFilter, value])
-  //       }
-  //       if (authorFilter.length !== 0 && !authorFilter.includes(value)) {
-  //         // setFilterTracks([
-  //         //   ...filterTracks,
-  //         //   ...filterAuthor(
-  //         //     data.filter((track) => genreFilter.indexOf(track.genre) > -1),
-  //         //     value,
-  //         //   ),
-  //         // ])
-  //         setAuthorFilter([...authorFilter, value])
-  //       }
-  //     }
-  //   } else if (type === 'genre') {
-  //     if (authorFilter.length === 0) {
-  //       if (genreFilter.includes(value)) {
-  //         // setFilterTracks(filterTracks.filter(({ genre }) => genre !== value))
-  //         setGenreFilter(genreFilter.filter((item) => item !== value))
-  //       } else {
-  //         // setFilterTracks([...filterTracks, ...filterGenre(data, value)])
-  //         setGenreFilter([...genreFilter, value])
-  //       }
-  //     }
-
-  //     if (authorFilter.length !== 0) {
-  //       if (genreFilter.includes(value)) {
-  //         // setFilterTracks(filterTracks.filter(({ genre }) => genre !== value))
-  //         setGenreFilter(genreFilter.filter((item) => item !== value))
-  //         if (genreFilter.length === 1) {
-  //           // setFilterTracks(
-  //           //   data.filter((track) => authorFilter.indexOf(track.author) > -1),
-  //           // )
-  //         }
-  //       }
-  //       if (genreFilter.length === 0) {
-  //         // setFilterTracks(filterGenre(filterTracks, value))
-  //         setGenreFilter([...genreFilter, value])
-  //       }
-  //       if (genreFilter.length !== 0 && !genreFilter.includes(value)) {
-  //         // setFilterTracks([
-  //         //   ...filterTracks,
-  //         //   ...filterGenre(
-  //         //     data.filter((track) => authorFilter.indexOf(track.author) > -1),
-  //         //     value,
-  //         //   ),
-  //         // ])
-  //         setGenreFilter([...genreFilter, value])
-  //       }
-  //     }
-  //   }
-  // }
-
   const handleSort = (value) => {
     if (value !== 'По умолчанию') {
       setPlaylist(sortTracks(playlist, value))
@@ -133,68 +50,72 @@ export const MainPage = () => {
     logOut()
   }
 
-  // const filterPlaylist = () => {
-  //   setFilterTracks(data.filter(({ author }) => authorFilter.includes(author)))
-  //   console.log('filetr', filterTracks)
-  //   // if (authorFilter.length > 0) {
-  //   return
-  //   // }
-  // }
-
   useEffect(() => {
-    if (search) {
-      if (authorFilter.length !== 0) {
-        // setFilterTracks(
-        //   data.filter(({ author }) => authorFilter.includes(author)),
-        // )
-        setPlaylist(searchMusic(data.filter(({ author }) => authorFilter.includes(author)), search))
-        setDefaultPlaylist(searchMusic(data.filter(({ author }) => authorFilter.includes(author)), search))
+    if (!search) {
+      if (authorFilter.length !== 0 && genreFilter.length === 0) {
+        setPlaylist(data.filter(({ author }) => authorFilter.includes(author)))
+        setDefaultPlaylist(
+          data.filter(({ author }) => authorFilter.includes(author)),
+        )
+      } else if (authorFilter.length !== 0 && genreFilter.length !== 0) {
+        setPlaylist(
+          data.filter(({ author, genre }) => {
+            return genreFilter.includes(genre) && authorFilter.includes(author)
+          }),
+        )
+      } else if (genreFilter.length !== 0 && authorFilter.length === 0) {
+        setPlaylist(data.filter(({ genre }) => genreFilter.includes(genre)))
+        setDefaultPlaylist(
+          data.filter(({ genre }) => genreFilter.includes(genre)),
+        )
+      } else {
+        setPlaylist(data)
+        setDefaultPlaylist(data)
       }
-      if (authorFilter.length === 0) {
+    } else if (search) {
+      if (authorFilter.length !== 0 && genreFilter.length === 0) {
+        setPlaylist(
+          searchMusic(
+            data.filter(({ author }) => authorFilter.includes(author)),
+            search,
+          ),
+        )
+        setDefaultPlaylist(
+          searchMusic(
+            data.filter(({ author }) => authorFilter.includes(author)),
+            search,
+          ),
+        )
+      } else if (authorFilter.length !== 0 && genreFilter.length !== 0) {
+        setPlaylist(
+          searchMusic(
+            data.filter(({ author, genre }) => {
+              return (
+                genreFilter.includes(genre) && authorFilter.includes(author)
+              )
+            }),
+            search,
+          ),
+        )
+      } else if (genreFilter.length !== 0 && authorFilter.length === 0) {
+        setPlaylist(
+          searchMusic(
+            data.filter(({ genre }) => genreFilter.includes(genre)),
+            search,
+          ),
+        )
+        setDefaultPlaylist(
+          searchMusic(
+            data.filter(({ genre }) => genreFilter.includes(genre)),
+            search,
+          ),
+        )
+      } else {
         setPlaylist(searchMusic(data, search))
         setDefaultPlaylist(searchMusic(data, search))
       }
     }
-    if (!search) {
-      if (authorFilter.length !== 0) {
-        // setFilterTracks(
-        //   data.filter(({ author }) => authorFilter.includes(author)),
-        // )
-        setPlaylist(data.filter(({ author }) => authorFilter.includes(author)))
-        setDefaultPlaylist(data.filter(({ author }) => authorFilter.includes(author)))
-      }
-      if (authorFilter.length === 0) {
-        setPlaylist(data)
-        setDefaultPlaylist(data)
-      }
-    } else {
-      setPlaylist(data)
-      setDefaultPlaylist(data)
-    }
-  }, [search, authorFilter, data])
-
-  // useEffect(() => {
-  //   if (authorFilter.length !== 0 && !search) {
-  //     setPlaylist(filterTracks)
-  //     setDefaultPlaylist(filterTracks)
-  //   } else if (!search && filterTracks.length === 0) {
-  //     setPlaylist(data)
-  //     setDefaultPlaylist(data)
-  //   } else if (search && filterTracks.length !== 0) {
-  //     setPlaylist(searchMusic(filterTracks, search))
-  //     setDefaultPlaylist(searchMusic(filterTracks, search))
-  //   } else if (search && filterTracks.length === 0) {
-  //     setPlaylist(searchMusic(data, search))
-  //     setDefaultPlaylist(searchMusic(data, search))
-  //   } else {
-  //     setPlaylist(data)
-  //     setDefaultPlaylist(data)
-  //     // console.log('playlist3',playlist);
-  //   }
-  // }, [search, filterTracks, data])
-
-  // console.log('authorFilter', authorFilter)
-  // console.log('filterTracks2',filterTracks);
+  }, [search, authorFilter, genreFilter, data])
 
   return (
     <>
@@ -205,9 +126,7 @@ export const MainPage = () => {
         <ErrorMessage allTracksError={allTracksError} />
       ) : (
         <>
-          {search &&
-          searchMusic(data, search).length === 0 &&
-          searchMusic(filterTracks, search).length === 0 ? (
+          {search && searchMusic(data, search).length === 0 ? (
             <h2>Ничего не найдено</h2>
           ) : (
             <TrackList tracks={playlist} />
